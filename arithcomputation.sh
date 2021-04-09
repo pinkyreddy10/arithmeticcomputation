469bb2194d1e4d41f30eca249eb40962e1bdb375
pinkyreddy10 <prashureddy97@gmail.com>
2021-04-09 13:02:51 +0530

    [Prasanthi] changed arithcomputation.sh file

--- arithcomputation.sh
+++ arithcomputation.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 declare -A dict
-declare -A codes
+declare -a array
 a=4
 b=2
 c=6
@@ -8,10 +8,8 @@ res1=$(( a+b*c ))
 res2=$(( a*b+c ))
 res3=$(( c+a/b ))
 res4=$(( a%b+c ))
-dict=( ["0"]="res1" ["1"]="res2" ["2"]="res3" ["3"]="res4" )
-codes=( ["res1"]="$(( a+b*c ))" ["res2"]="$(( a*b+c ))" ["res3"]="$(( c+a/b ))" ["res4"]="$(( a%b+c ))" )
-
-for key in ${!dict[@]}
+dict=( [0]=$res1 [1]=$res2 [2]=$res3 [3]=$res4 )
+for (( i=0; i<4; i++ ))
 do
-	echo $key ${codes[${dict[$key]}]}
+	array[$i]=${dict[$i]}
 done
